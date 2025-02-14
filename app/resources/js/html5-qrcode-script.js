baa57c0d6ba4e83e3f5e115bbbe8051350d1a4f1
dhikaxzz <afdhikasyahputra@gmail.com>
2025-02-14 10:29:33 +0700

    update scan

--- app/resources/js/html5-qrcode-script.js
+++ app/resources/js/html5-qrcode-script.js
@@ -1,22 +1,33 @@
 console.log("halo semua");
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('qrScanner', () => ({
         scannedResult: '',
+        qrCodeReader: null,
 
         initScanner() {
-            const qrCodeReader = new Html5Qrcode("qr-reader");
-            qrCodeReader.start(
+            this.qrCodeReader = new Html5Qrcode("qr-reader");
+            this.qrCodeReader.start(
                 { facingMode: "environment" }, // Kamera belakang
                 { fps: 10, qrbox: 250 },
                 (decodedText) => {
                     this.scannedResult = decodedText;
-                    alert('Hasil Scan: ' + decodedText);
-                    qrCodeReader.stop();
+                    alert('Kode Barang: ' + decodedText);
+                    this.destroyScanner(); // Matikan scanner setelah scan berhasil
                 },
                 (error) => {
                     console.error(error);
                 }
             );
+        },
+
+        destroyScanner() {
+            if (this.qrCodeReader) {
+                this.qrCodeReader.stop().then(() => {
+                    console.log("QR Scanner Stopped");
+                }).catch(err => console.error("Error stopping QR scanner:", err));
+                this.qrCodeReader = null;
+            }
         }
     }));
 });
--- resources/views/components/scan-qr.blade.php
+++ resources/views/components/scan-qr.blade.php
@@ -1,4 +1,16 @@
-<div x-data="qrScanner" x-init="initScanner()" class="p-4">
+<div x-data="qrScanner" x-init="initScanner()" 
+     @keydown.escape.window="destroyScanner()" 
+     @close.window="destroyScanner()" 
+     class="p-4">
     <div id="qr-reader" class="border rounded-lg"></div>
-    <p x-text="scannedResult" class="mt-2 text-lg font-bold text-center"></p>
+    
+    <!-- Teks "Kode Barang :" + Hasil Scan -->
+    <p class="mt-2 text-lg font-bold text-center">
+        Kode Barang: <span x-text="scannedResult"></span>
+    </p>
+
+    <!-- Tombol Tutup Modal -->
+    <button @click="$dispatch('close')" class="mt-4 bg-red-500 text-white px-4 py-2 rounded">
+        Tutup Modal
+    </button>
 </div>
