9d060891c2310bc5acb10775a38993851fadff88
dhikaxzz <afdhikasyahputra@gmail.com>
2025-03-13 13:11:31 +0700

    searchable

--- app/Filament/Resources/BarangResource.php
+++ app/Filament/Resources/BarangResource.php
@@ -133,11 +133,10 @@ class BarangResource extends Resource
         return $table->columns([
             Tables\Columns\TextColumn::make('kode_barang')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('nama_barang')->sortable()->searchable(),
-            Tables\Columns\TextColumn::make('kategori.nama_kategori')->label('Kategori')->sortable(),
+            Tables\Columns\TextColumn::make('kategori.nama_kategori')->label('Kategori')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('merek')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('kondisi')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('lokasi')->sortable()->searchable(),
-            // Tables\Columns\TextColumn::make('model_seri')->label('Model/Seri')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('status')
                 ->badge()
                 ->color(fn (string $state): string => match ($state) {
