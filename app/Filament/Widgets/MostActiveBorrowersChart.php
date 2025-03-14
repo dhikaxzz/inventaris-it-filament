33c51493c718efc0bb64dd88bc7ef52ee6b7760c
dhikaxzz <afdhikasyahputra@gmail.com>
2025-03-14 11:07:08 +0700

    update tampilan chart

--- app/Filament/Widgets/MostActiveBorrowersChart.php
+++ app/Filament/Widgets/MostActiveBorrowersChart.php
@@ -7,11 +7,12 @@ use App\Models\Peminjaman;
 
 class MostActiveBorrowersChart extends ChartWidget
 {
-    protected static ?string $heading = 'Pengguna Paling Banyak Meminjam Barang';
+    protected static ?string $heading = 'Pengguna dengan Peminjaman Terbanyak';
+    protected static ?int $sort = 2; // Menentukan urutan widget jika ada beberapa
 
     protected function getData(): array
     {
-        // Ambil 5 peminjam dengan jumlah peminjaman terbanyak
+        // Mengambil 5 pengguna dengan jumlah peminjaman terbanyak
         $topUsers = Peminjaman::selectRaw('nama_peminjam, COUNT(*) as total_peminjaman')
             ->groupBy('nama_peminjam')
             ->orderByDesc('total_peminjaman')
@@ -23,12 +24,14 @@ class MostActiveBorrowersChart extends ChartWidget
                 [
                     'label' => 'Total Peminjaman',
                     'data' => $topUsers->pluck('total_peminjaman')->toArray(),
+                    'backgroundColor' => ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+                    'borderWidth' => 1,
                 ],
             ],
             'labels' => $topUsers->pluck('nama_peminjam')->toArray(),
         ];
     }
-    
+
     protected function getType(): string
     {
         return 'bar';
